147c67e70fef62fa54fde5247448dc64a0cb677f
Vinicius Krieger <viniheiko@gmail.com>
2024-08-05 13:29:23 -0300

    Criando o rodapé

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,50 @@
+import { Container, FooterSection, Link, Links, SectionTitle } from './styles'
+
+const CurrentYear = new Date().getFullYear()
+
+const Footer = () => (
+  <Container>
+    <div className="container">
+      <FooterSection>
+        <SectionTitle>Categorias</SectionTitle>
+        <Links>
+          <li>
+            <Link>RPG</Link>
+          </li>
+          <li>
+            <Link>Ação</Link>
+          </li>
+          <li>
+            <Link>Aventura</Link>
+          </li>
+          <li>
+            <Link>Esporte</Link>
+          </li>
+          <li>
+            <Link>FPS</Link>
+          </li>
+          <li>
+            <Link>Simulação</Link>
+          </li>
+        </Links>
+      </FooterSection>
+      <FooterSection>
+        <SectionTitle>Acesso Rápido</SectionTitle>
+        <Links>
+          <li>
+            <Link>Novidades</Link>
+          </li>
+          <li>
+            <Link>Promoções</Link>
+          </li>
+          <li>
+            <Link>Em Breve</Link>
+          </li>
+        </Links>
+      </FooterSection>
+      <p>{CurrentYear} - &copy; E-Play todos os direitors reservados</p>
+    </div>
+  </Container>
+)
+
+export default Footer
